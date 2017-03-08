2ba1f8f4571b9ac8bc61ef397a11285b33e18893
mzarako <monika.zarako@gmail.com>
2017-03-08 07:05:40 -0800

    updated aboutme statement

--- src/content/about.js
+++ src/content/about.js
@@ -1,7 +1,7 @@
 const introParagraph = 'Hello, I am a web developer based in Los Angeles. I enjoy all aspects of development, from concept, design, frontend, and backend. With a background in music, physics, and architecture, I enjoy bringing together varied perscpecives and skills to build both interesting and useful products. Learn more about me below, and email me if you would like to talk.';
 
 const aboutMe = {
-  intro: 'I am a man of many passions, among which are music, physics, mathematics and architecture. Before learning web and app development, I was working as a composer for the entertainment industry and a residential home designer.',
+  intro: 'I am a man of many passions, among which are music, physics, mathematics and architecture. Before learning web and app development I studied physics at UC Santa Barabara, worked as a composer for the entertainment industry and as a residential home designer.',
   likes: 'Sci-Fi Literature, Ancient History, Board Games, Sequoia Trees, Southern Utah, Traveling, and the Lakers',
   musicIntro: 'I happen to be a trained orchestral composer and have a very close relationship with the guitar. Some of my music on is featured on SpongeBob SquarePants.',
   musicLikes: 'Gustav Holtz - The Planets, Gustav Mahler - 2nd Symphony, Claude Debussy - La Mer',
